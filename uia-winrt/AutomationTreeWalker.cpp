9c514c7e10118047592cb2c0fdc64118c56c9066
Remi Pelletier <remipelletier5@gmail.com>
2021-09-25 01:32:10 -0400

    Added AutomationTreeWalker

---
+++ uia-winrt/AutomationTreeWalker.cpp
@@ -0,0 +1,121 @@
+﻿#include "pch.h"
+#include "AutomationTreeWalker.h"
+#if __has_include("AutomationTreeWalker.g.cpp")
+#include "AutomationTreeWalker.g.cpp"
+#endif
+
+#include "AutomationCacheRequest.h";
+#include "AutomationCondition.h";
+#include "AutomationElement.h"
+
+namespace winrt::uia::implementation
+{
+    uia::AutomationCondition AutomationTreeWalker::Condition()
+    {
+        com_ptr<IUIAutomationCondition> uiAutomationCondition;
+        check_hresult(m_uiAutomationTreeWalker->get_Condition(uiAutomationCondition.put()));
+        return winrt::make<implementation::AutomationCondition>(uiAutomationCondition);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetFirstChildElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> firstChildElement;
+        check_hresult(m_uiAutomationTreeWalker->GetFirstChildElement(uiAutomationElement.get(), firstChildElement.put()));
+        return winrt::make<implementation::AutomationElement>(firstChildElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetFirstChildElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> firstChildElement;
+        check_hresult(m_uiAutomationTreeWalker->GetFirstChildElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), firstChildElement.put()));
+        return winrt::make<implementation::AutomationElement>(firstChildElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetLastChildElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> lastChildElement;
+        check_hresult(m_uiAutomationTreeWalker->GetLastChildElement(uiAutomationElement.get(), lastChildElement.put()));
+        return winrt::make<implementation::AutomationElement>(lastChildElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetLastChildElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> lastChildElement;
+        check_hresult(m_uiAutomationTreeWalker->GetLastChildElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), lastChildElement.put()));
+        return winrt::make<implementation::AutomationElement>(lastChildElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetNextSiblingElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> nextSiblingElement;
+        check_hresult(m_uiAutomationTreeWalker->GetNextSiblingElement(uiAutomationElement.get(), nextSiblingElement.put()));
+        return winrt::make<implementation::AutomationElement>(nextSiblingElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetNextSiblingElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> nextSiblingElement;
+        check_hresult(m_uiAutomationTreeWalker->GetNextSiblingElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), nextSiblingElement.put()));
+        return winrt::make<implementation::AutomationElement>(nextSiblingElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetParentElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> parentElement;
+        check_hresult(m_uiAutomationTreeWalker->GetParentElement(uiAutomationElement.get(), parentElement.put()));
+        return winrt::make<implementation::AutomationElement>(parentElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetParentElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> parentElement;
+        check_hresult(m_uiAutomationTreeWalker->GetParentElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), parentElement.put()));
+        return winrt::make<implementation::AutomationElement>(parentElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetPreviousSiblingElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> previousSiblingElement;
+        check_hresult(m_uiAutomationTreeWalker->GetPreviousSiblingElement(uiAutomationElement.get(), previousSiblingElement.put()));
+        return winrt::make<implementation::AutomationElement>(previousSiblingElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::GetPreviousSiblingElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> previousSiblingElement;
+        check_hresult(m_uiAutomationTreeWalker->GetPreviousSiblingElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), previousSiblingElement.put()));
+        return winrt::make<implementation::AutomationElement>(previousSiblingElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::NormalizeElement(uia::AutomationElement element)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationElement> normalizedElement;
+        check_hresult(m_uiAutomationTreeWalker->NormalizeElement(uiAutomationElement.get(), normalizedElement.put()));
+        return winrt::make<implementation::AutomationElement>(normalizedElement);
+    }
+
+    uia::AutomationElement AutomationTreeWalker::NormalizeElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest)
+    {
+        com_ptr<IUIAutomationElement> uiAutomationElement = winrt::get_self<implementation::AutomationElement>(element)->UiAutomationElement();
+        com_ptr<IUIAutomationCacheRequest> uiAutomationCacheRequest = winrt::get_self<implementation::AutomationCacheRequest>(cacheRequest)->UiAutomationCacheRequest();
+        com_ptr<IUIAutomationElement> normalizedElement;
+        check_hresult(m_uiAutomationTreeWalker->NormalizeElementBuildCache(uiAutomationElement.get(), uiAutomationCacheRequest.get(), normalizedElement.put()));
+        return winrt::make<implementation::AutomationElement>(normalizedElement);
+    }
+}
---
+++ uia-winrt/AutomationTreeWalker.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "AutomationTreeWalker.g.h"
+
+namespace winrt::uia::implementation
+{
+    struct AutomationTreeWalker : AutomationTreeWalkerT<AutomationTreeWalker>
+    {
+        AutomationTreeWalker(com_ptr<IUIAutomationTreeWalker> const& uiAutomationTreeWalker)
+            : m_uiAutomationTreeWalker(uiAutomationTreeWalker) {}
+
+        com_ptr<IUIAutomationTreeWalker> UiAutomationCacheRequest() const noexcept { return m_uiAutomationTreeWalker; }
+
+        uia::AutomationCondition Condition();
+
+        uia::AutomationElement GetFirstChildElement(uia::AutomationElement element);
+        uia::AutomationElement GetFirstChildElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+        uia::AutomationElement GetLastChildElement(uia::AutomationElement element);
+        uia::AutomationElement GetLastChildElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+        uia::AutomationElement GetNextSiblingElement(uia::AutomationElement element);
+        uia::AutomationElement GetNextSiblingElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+        uia::AutomationElement GetParentElement(uia::AutomationElement element);
+        uia::AutomationElement GetParentElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+        uia::AutomationElement GetPreviousSiblingElement(uia::AutomationElement element);
+        uia::AutomationElement GetPreviousSiblingElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+        uia::AutomationElement NormalizeElement(uia::AutomationElement element);
+        uia::AutomationElement NormalizeElementBuildCache(uia::AutomationElement element, uia::AutomationCacheRequest cacheRequest);
+
+    private:
+        com_ptr<IUIAutomationTreeWalker> m_uiAutomationTreeWalker;
+    };
+}
