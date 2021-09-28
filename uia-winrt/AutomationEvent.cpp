174248dcb53d2dbc3dbe4b07b41ebfdd70fb3d57
Remi Pelletier <remipelletier5@gmail.com>
2021-09-28 01:41:03 -0400

    Added AutomationEvent draft

---
+++ uia-winrt/AutomationEvent.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "AutomationEvent.h"
+#if __has_include("AutomationEvent.g.cpp")
+#include "AutomationEvent.g.cpp"
+#endif
+
+namespace winrt::uia::implementation
+{
+	uia::AutomationEvent AutomationEvent::LookupById(int eventId)
+	{
+		// Perform ID validation?
+		return winrt::make<implementation::AutomationEvent>(eventId);
+	}
+}
---
+++ uia-winrt/AutomationEvent.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "AutomationEvent.g.h"
+
+namespace winrt::uia::implementation
+{
+    struct AutomationEvent : AutomationEventT<AutomationEvent>
+    {
+        AutomationEvent(int id) : m_id(id) {}
+
+        int Id() const noexcept { return m_id; }
+
+        static uia::AutomationEvent LookupById(int eventId);
+
+    private:
+        int m_id;
+    };
+}
+
+namespace winrt::uia::factory_implementation
+{
+    struct AutomationEvent : AutomationEventT<AutomationEvent, implementation::AutomationEvent>
+    {
+    };
+}
